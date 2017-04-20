a8e90acfc74d77991b50281a85698b566bdf835c
root <hellachikhaoui@gmail.com>
2017-04-20 22:12:39 +0100

    movies

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('./src/lib/app')
 app.listen(3000)
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect('mongodb://127.0.0.1:27017/movies', function (err, db) {
+MongoClient.connect(process.env.PROD_MONGODB, function (err, db) {
     if (err) {
         throw err;
     } else {
