16ee006ff46f3106e5532f7286423477254df214
root <hellachikhaoui@gmail.com>
2017-04-20 22:40:26 +0100

    movies

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var app = require('./src/lib/app')
 app.listen(process.env.PORT||3000)
 // app.set('port', (process.env.PORT || 5000));
- app.use(express.static(__dirname + '/public'));
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 MongoClient.connect(process.env.PROD_MONGODB, function (err, db) {
