e303f7a6d78f44c0c907d58f25e58465f8b25a23
root <hellachikhaoui@gmail.com>
2017-04-21 14:16:04 +0100

    movies

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var app = require('./src/lib/app')
-var port = process.env.PORT || 8000
-app.listen(port||3000)
-// app.set('port', (process.env.PORT || 5000));
+app.listen(3000)
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect(process.env.PROD_MONGODB, function (err, db) {
+MongoClient.connect('mongodb://127.0.0.1:27017/movies', function (err, db) {
     if (err) {
         throw err;
     } else {
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoUrl = process.env.PROD_MONGODB;
+var mongoUrl = 'mongodb://127.0.0.1:27017/movies';
 
 module.exports = function(app) {
   mongoose.connect(mongoUrl, {
