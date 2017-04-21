fd6a923ecb756676fbfee09efa6df18b3932c56f
root <hellachikhaoui@gmail.com>
2017-04-21 13:51:29 +0100

    movies

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ app.listen(process.env.PORT||3000)
 // app.set('port', (process.env.PORT || 5000));
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect(process.env.PROD_MONGODB, function (err, db) {
+MongoClient.connect(process.env.PROD_MONGODB+'?authMode=scram-sha1', function (err, db) {
     if (err) {
         throw err;
     } else {
