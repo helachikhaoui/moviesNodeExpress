b239f231e5fa4aefb6be0f0693536f5774aa7cf3
root <hellachikhaoui@gmail.com>
2017-04-22 11:46:35 +0100

    movies

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoUrl = 'mongodb://127.0.0.1:27017/movies'||process.env.PROD_MONGODB;
+var mongoUrl = process.env.PROD_MONGODB||'mongodb://127.0.0.1:27017/movies'||process.env.PROD_MONGODB;
 
 module.exports = function(app) {
   mongoose.connect(mongoUrl, {
