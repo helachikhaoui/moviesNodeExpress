7ceda725c990aa5a431de9b5e5c2b5bf2f873314
root <hellachikhaoui@gmail.com>
2017-05-05 13:52:25 +0100

    tp2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ app.listen(process.env.PORT||3000)
 // app.set('port', (process.env.PORT || 5000));
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
-MongoClient.connect(process.env.PROD_MONGODB, function (err, db) {
+MongoClient.connect('mongodb://127.0.0.1:27017/movies', function (err, db) {
     if (err) {
         throw err;
     } else {
